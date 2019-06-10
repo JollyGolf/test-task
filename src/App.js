b5aea4bd79c5f2eeb2e430825dd90f7fe01af4a4
jollygolf <zhigamovky>
2019-06-10 17:54:04 +0300

    Created Tabs v.0.2 [Changed class to functional component]

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import Tabs from './Tabs/TabHOC';
 
 const App = () => (
-  <Container>
-    <Tabs />
-  </Container>
+  <Tabs />
 );
 
 export default App;
-
-const Container = styled.div`
-  display: flex;
-  felx-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  background-color: lightgrey;
-`
--- src/Tabs/TabHOC.js
+++ src/Tabs/TabHOC.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Tab1 from './Tab1';
 import Tab2 from './Tab2';
@@ -20,29 +21,65 @@ const withHOC = activeTabIndex => {
   return Component;
 }
 
-class TabFunctional extends React.Component {
-  constructor() {
-    super()
-    this.state = { activeTabIndex: 1 }
-    this.changeTab = this.changeTab.bind(this)
-  }
-  
-  changeTab(index) {
-    this.setState({ activeTabIndex: index });
-  }
-
-  render() {
-    const { activeTabIndex } = this.state;
-    const Content = withHOC(activeTabIndex);
-    return (
-      <div>
-        <TabLabel active={activeTabIndex === 1} onClick={this.changeTab.bind(this, 1)}>Tab 1</TabLabel>
-        <TabLabel active={activeTabIndex === 2} onClick={this.changeTab.bind(this, 2)}>Tab 2</TabLabel>
-        <TabLabel active={activeTabIndex === 3} onClick={this.changeTab.bind(this, 3)}>Tab 3</TabLabel>
-        <Content /> 
-      </div>
-    )
-  }
+const TabFunctional = () => {
+  const [activeTabIndex, setActiveTabIndex] = useState(1);
+  const Content = withHOC(activeTabIndex);
+  const changeTab = index => () => setActiveTabIndex(index);
+  return (
+    <Container>
+      <Navigation>
+        <TabLabel active={activeTabIndex === 1} onClick={ changeTab(1) }>Tab 1</TabLabel>
+        <TabLabel active={activeTabIndex === 2} onClick={ changeTab(2) }>Tab 2</TabLabel>
+        <TabLabel active={activeTabIndex === 3} onClick={ changeTab(3) }>Tab 3</TabLabel>
+      </Navigation>
+      <Content /> 
+    </Container>
+  );
 }
 
 export default TabFunctional;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  justifyContent: center;
+  margin: 0 auto;
+  font-size: 15pt;
+`
+
+const Navigation = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: lightgrey;
+`
+
+
+// class TabFunctional extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = { activeTabIndex: 1 }
+//     this.changeTab = this.changeTab.bind(this)
+//   }
+  
+//   changeTab(index) {
+//     this.setState({ activeTabIndex: index });
+//   }
+
+//   render() {
+//     const { activeTabIndex } = this.state;
+//     const Content = withHOC(activeTabIndex);
+//     return (
+//       <Container>
+//         <Navigation>
+//           <TabLabel active={activeTabIndex === 1} onClick={this.changeTab.bind(this, 1)}>Tab 1</TabLabel>
+//           <TabLabel active={activeTabIndex === 2} onClick={this.changeTab.bind(this, 2)}>Tab 2</TabLabel>
+//           <TabLabel active={activeTabIndex === 3} onClick={this.changeTab.bind(this, 3)}>Tab 3</TabLabel>
+//         </Navigation>
+//         <Content /> 
+//       </Container>
+//     )
+//   }
+// }
